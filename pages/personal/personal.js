08be931bdf7453d4bd7e87b799a6b3b336eb141a
sbill <1422347728@qq.com>
2020-06-18 11:26:35 +0800

    基本页面和最近播放
    搜索页和播放详情页的结构和样式，大概编写完成了；从推荐页播放的歌曲，通过缓存，能够在最近播放那里显示了

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -4,16 +4,29 @@ Page({
   data: {
     music: {},
     showPlayer: false,
-    isPlaying: false
+    isPlaying: false,
+    palyedMusics: {},
+    palyedMusicsid: []
+  },
+  onLoad(){
+    
   },
   onShow() {
+    // 获取最近播放的歌曲
+    let palyedMusicsid = wx.getStorageSync('musicsId');
+    let palyedMusics = wx.getStorageSync('musicsMap');
+
+    //获取正在播放的歌曲
     let music = wx.getStorageSync('music');
     let showPlayer = app.globalData.showPlayer;
     let isPlaying = app.globalData.isPlaying;
+
     this.setData({
       music: music,
       showPlayer: showPlayer,
-      isPlaying: isPlaying
+      isPlaying: isPlaying,
+      palyedMusics: palyedMusics,
+      palyedMusicsid: palyedMusicsid
     });
   },
   onControlTap(event) {
--- pages/recommend/recommend.js
+++ pages/recommend/recommend.js
@@ -1,4 +1,6 @@
-import {data} from './music.js';
+import {
+  data
+} from './music.js';
 const app = getApp();
 
 Page({
@@ -9,8 +11,6 @@ Page({
     music: {}
   },
   onLoad(options) {
-    console.log('recommend load');
-
     this.setData(data); // 将本地数据放到 data 中
 
     // 在公共数据那边保存音乐是否播放的状态，以此来决定是否显示本页的播放器
@@ -22,27 +22,33 @@ Page({
   },
   onShow() {
     this.setData({
-      isPlaying: app.globalData.isPlaying
+      isPlaying: app.globalData.isPlaying //及时更新状态，绑定变量那边需要
     });
   },
   onMusicTap(event) {
+    // 列表渲染时，可以知道一首歌的全部信息
     const music = event.currentTarget.dataset.music;
+
+    wx.navigateTo({
+      url: 'detail/detail'
+    });
 
     // 如果歌曲正在播放，就不要执行播放的函数了
     if (this.data.isPlaying) {
-      if (this.data.music.url === music.url){
-        return;        
+      if (this.data.music.url === music.url) {
+        return;
       }
     }
     this.playMusic(music);
     this.setData({
       music: music
     });
-    wx.setStorageSync('music', music);
-    wx.setStorageSync('isPlaying', true);
+    wx.setStorageSync('music', music); // 给personal页面底部使用，不使用全局变量，使用缓存试试
+    this.recordPlay(music);
+
   },
   onControlTap(event) {
-    if (this.data.isPlaying){
+    if (this.data.isPlaying) {
       wx.pauseBackgroundAudio();
       this.setData({
         isPlaying: false
@@ -56,7 +62,17 @@ Page({
       app.globalData.isPlaying = true;
     }
   },
-  playMusic({name, singer, imgUrl, url}) {
+  onSearchTap(event) {
+    wx.navigateTo({
+      url: 'search/search'
+    })
+  },
+  playMusic({
+    name,
+    singer,
+    imgUrl,
+    url
+  }) {
     const backgroundAudioManager = wx.getBackgroundAudioManager();
     backgroundAudioManager.title = name;
     backgroundAudioManager.epname = name;
@@ -64,11 +80,45 @@ Page({
     backgroundAudioManager.coverImgUrl = imgUrl;
     // 设置了 src 之后会自动播放
     backgroundAudioManager.src = url;
+
+    // 及时更新状态
     this.setData({
       isPlaying: true,
       showPlayer: true
     });
     app.globalData.showPlayer = true;
     app.globalData.isPlaying = true;
+  },
+  recordPlay(music) {
+    // 添加到缓存，记录最近播放的歌曲
+    // 使用 set 对象来确保 id 不会重复
+    let musicsMap = null;
+    let musicsId = null;
+    if (wx.getStorageSync('musicsMap')) {
+      musicsMap = wx.getStorageSync('musicsMap');
+    } else {
+      musicsMap = {};
+    }
+    if (wx.getStorageSync('musicsId')) {
+      musicsId = wx.getStorageSync('musicsId');
+    } else {
+      musicsId = [];
+    }
+
+    let musicsIdSet = new Set(musicsId);
+    if (!musicsIdSet.has(music.id)) {
+      // 如果歌曲没有播放过
+      musicsMap[music.id] =  music;
+      wx.setStorageSync('musicsMap', musicsMap);
+
+      musicsId.unshift(music.id);
+      wx.setStorageSync('musicsId', musicsId);
+    } else {
+      // 利用 set 对象来去重
+      musicsId.unshift(music.id);
+      musicsIdSet = new Set(musicsId);
+      musicsId = [...musicsIdSet];
+      wx.setStorageSync('musicsId', musicsId);
+    }
   }
 })
